12df570a05f389f8fe34c68c998e642b885c136a
mamigako <maxklmko@gmail.com>
2025-02-07 17:46:37 +0000

    interpreter fetch and decode methods complete.

--- Classes/SInterpreter.py
+++ Classes/SInterpreter.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class SInterpreter:
 
 
@@ -8,16 +11,54 @@ class SInterpreter:
 
     def cycle(self):
         #performs the fetch-decode-execute cycle
-        pass
 
-    def fetch():
-        pass
+        next_line = self.fetch()
 
-    def decode():
-        pass
+        operation = self.decode(next_line)
 
-    def execute():
-        pass
+        self.execute(operation)
+
+
+    def fetch(self):
+
+        operation = sys.stdin.readline()
+        return operation
+    
+
+    def decode(self, next_line):
+
+        if "PUSH" in next_line:
+
+            next_line = next_line[5:]
+
+            if next_line.isdigit():
+                next_line = int(next_line)
+                self.stack.append(next_line)
+                return "PUSH"
+            
+            elif next_line.isalpha():
+                self.stack.append(next_line)
+                self.values[next_line] = 0
+                return "PUSH"
+            
+        else:
+            return next_line
+
+
+
+    def execute(self, operation):
+
+        match operation:
+            case "PUSH":
+                return
+            case "ADD":
+                return
+            case "MULT":
+                return
+            case "ASSIGN":
+                return
+            case "PRINT":
+                return
 
 
 
